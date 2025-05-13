26f83ec0017a6477afb39f37bd389136265a21fa
SJarod <jarsengkeoPro@gmail.com>
2025-05-14 00:33:22 +0200

    probes 3d wip

---
+++ src/client/scripts/radiance_cascades3d.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+
+#include "graphics/device.hpp"
+
+#include "radiance_cascades3d.hpp"
+
+int RadianceCascades3D::getTotalProbeCount(std::vector<cascade> cascades) const
+{
+    int probeCount = 0;
+    for (int i = 0; i < cascades.size(); ++i)
+    {
+        probeCount += cascades[i].probes.size();
+    }
+    return probeCount;
+}
+
+RadianceCascades3D::cascade RadianceCascades3D::createCascade(cascade_desc cd) const
+{
+    cascade result(cd.p);
+    result.desc = cd;
+
+    // probe count per dimension
+    float px = sqrt(float(cd.p));
+    float py = px;
+    float ipx = 1.0 / px;
+    float ipy = 1.0 / py;
+
+    // center the probes
+    glm::vec2 offset = glm::vec2(ipx * -0.5, ipy * -0.5);
+
+    // probes within  the cascade
+    for (float i = 0.0; i < px; ++i)
+    {
+        for (float j = 0.0; j < py; ++j)
+        {
+            // initialize probe posititon
+            int probeIndex = int(px * i + j);
+            result.probes[probeIndex].position.x = ipx * float(i) - offset.x;
+            result.probes[probeIndex].position.y = ipy * float(j) - offset.y;
+        }
+    }
+
+    // radiance interval count
+    result.m = cd.p * cd.q;
+
+    return result;
+}
+std::vector<RadianceCascades3D::cascade> RadianceCascades3D::createCascades(cascade_desc desc0, int cascadeCount) const
+{
+    std::vector<cascade> result;
+    result.reserve(cascadeCount);
+
+    for (int i = 0; i < cascadeCount; ++i)
+    {
+        result.push_back(createCascade(desc0));
+        // P1 = P0/4
+        desc0.p /= 4;
+        // Q1 = 2Q0
+        desc0.q *= 2;
+        // DW1 = 2DW0
+        desc0.dw *= 2.0;
+    }
+
+    return result;
+}
+void RadianceCascades3D::init(void *userData)
+{
+    std::cout << "Radiance Cascades Init" << std::endl;
+
+    auto data = (init_data *)userData;
+    m_device = data->device;
+
+    {
+        BufferDirector bd;
+        BufferBuilder bb;
+        bd.configureUniformBufferBuilder(bb);
+        bb.setDevice(m_device);
+        bb.setSize(sizeof(parameters));
+        bb.setName("Radiance Cascades Parameters Buffer");
+
+        m_radianceCascadesParametersBuffer = bb.build();
+        parameters params = {
+            .maxCascadeCount = m_maxCascadeCount,
+            .maxProbeCount = m_maxProbeCount,
+            .minDiscreteValueCount = m_minDiscreteValueCount,
+            .minRadianceintervalLength = m_minRadianceintervalLength,
+            .lightIntensity = m_lightIntensity,
+            .maxRayIterationCount = m_maxRayIterationCount,
+        };
+        m_radianceCascadesParametersBuffer->copyDataToMemory(&params);
+    }
+
+    // create probes in a cascade
+
+    cascade_desc cd0 = cascade_desc(m_maxProbeCount, m_minDiscreteValueCount, m_minRadianceintervalLength);
+
+    // create cascades
+    auto cascades = createCascades(cd0, m_maxCascadeCount);
+
+    // buffer 1 is for the descriptors of each cascade used in the fragment shader
+    // all cascades descriptors
+    {
+        std::vector<cascade_desc> descs;
+        descs.reserve(cascades.size());
+        for (int i = 0; i < cascades.size(); ++i)
+        {
+            descs.push_back(cascades[i].desc);
+        }
+
+        BufferDirector bd;
+        BufferBuilder bb;
+        bd.configureStorageBufferBuilder(bb);
+        bb.setDevice(m_device);
+        bb.setSize(sizeof(cascade_desc) * cascades.size());
+        bb.setName("Radiance Cascades Descriptors Buffer");
+
+        m_cascadesDescBuffer = bb.build();
+        m_cascadesDescBuffer->copyDataToMemory(descs.data());
+    }
+
+    // buffer 2 is for all the actual probe data (positions)
+    // all cascades probes positions
+    {
+        // total number of probes combining all the cascades
+        int probeCount = 0;
+        std::vector<probe> positions;
+        for (int i = 0; i < cascades.size(); ++i)
+        {
+            probeCount += cascades[i].desc.p;
+
+            for (const probe &p : cascades[i].probes)
+            {
+                positions.push_back(p);
+            }
+        }
+
+        BufferDirector bd;
+        BufferBuilder bb;
+        bd.configureStorageBufferBuilder(bb);
+        bb.setDevice(m_device);
+        bb.setSize(sizeof(probe) * probeCount);
+        bb.setName("Radiance Cascades Positions Buffer");
+
+        m_probePositionBuffer = bb.build();
+        m_probePositionBuffer->copyDataToMemory(positions.data());
+    }
+
+    // buffer 3 is a storage buffer for the radiance gathering
+    // write : writting the radiance intervals in the gather phase
+    // read : fragment shader read the radiance intervals and merge and apply for the indirect lighting computation
+    {
+        for (int i = 0; i < data->frameInFlightCount; ++i)
+        {
+            BufferDirector bd;
+            BufferBuilder bb;
+            bd.configureStorageBufferBuilder(bb);
+            bb.setDevice(m_device);
+            bb.setName("Radiance Cascades Radiance Intervals Buffer");
+
+            // total number of interval combining all the cascades
+            int intervalCount = 0;
+            for (int i = 0; i < cascades.size(); ++i)
+            {
+                intervalCount += cascades[i].m;
+            }
+
+            bb.setSize(sizeof(glm::vec4) * intervalCount);
+
+            m_radianceIntervalsStorageBufferRW.push_back(bb.build());
+        }
+    }
+}
+
+void RadianceCascades3D::begin()
+{
+    std::cout << "Radiance Cascades Begin" << std::endl;
+}
+void RadianceCascades3D::update(float deltaTime)
+{
+}
---
+++ src/client/scripts/radiance_cascades3d.hpp
@@ -0,0 +1,139 @@
+#pragma once
+
+#include <cmath>
+#include <memory>
+#include <vector>
+
+#include <glm/glm.hpp>
+
+#include "graphics/buffer.hpp"
+
+#include "engine/scriptable.hpp"
+
+class Device;
+
+class RadianceCascades3D : public ScriptableABC
+{
+  public:
+    struct init_data
+    {
+        std::weak_ptr<Device> device;
+        uint32_t frameInFlightCount;
+    };
+
+  private:
+    const int m_maxCascadeCount = 3;
+    // p = probe count is a square number
+    const int m_maxProbeCount = 8 * 8;
+    // q = discrete value count will be doubled every cascade
+    // number of radiance intervals for the probes from first cascade
+    const int m_minDiscreteValueCount = 8;
+    // dw = radiance interval length will be doubled every cascade
+    // taken from https://www.shadertoy.com/view/mtlBzX
+    const float m_minRadianceintervalLength =
+        glm::vec2(1366, 768).length() * 4.0 / (float(1 << 2 * m_maxCascadeCount) - 1.0);
+
+    // intensity of every lights (when applying irradiance)
+    const float m_lightIntensity = 1.f;
+
+    // iteration along the ray for the raycasting function
+    // it is for now uniformly distributed (it may not be precise)
+    const int m_maxRayIterationCount = 32;
+
+    struct parameters
+    {
+        int maxCascadeCount;
+        int maxProbeCount;
+        int minDiscreteValueCount;
+        float minRadianceintervalLength;
+        float lightIntensity;
+        int maxRayIterationCount;
+        int pad[2];
+    };
+    std::unique_ptr<Buffer> m_radianceCascadesParametersBuffer;
+
+    struct probe
+    {
+        glm::vec3 position;
+        float pad;
+    };
+
+    struct cascade_desc
+    {
+        // number of probes p
+        uint32_t p;
+        // number of discrete values per probes q
+        uint32_t q;
+
+        // interval length
+        float dw;
+    };
+
+    struct cascade
+    {
+        cascade_desc desc;
+
+        std::vector<probe> probes;
+
+        // total of radiance intervals in the cascade
+        // m = pq
+        int m;
+
+        cascade() = delete;
+        explicit cascade(int probeCount)
+        {
+            probes.resize(probeCount);
+        }
+    };
+
+    std::weak_ptr<Device> m_device;
+
+    /**
+     * @brief buffer containing the cascades description
+     *
+     */
+    std::unique_ptr<Buffer> m_cascadesDescBuffer;
+    /**
+     * @brief buffer containing the probe information from the cascade
+     *
+     */
+    std::unique_ptr<Buffer> m_probePositionBuffer;
+    /**
+     * @brief buffer containing the probe radiance intervals information for write and read
+     * It is an array because there are as many as frames in flight.
+     *
+     */
+    std::vector<std::unique_ptr<Buffer>> m_radianceIntervalsStorageBufferRW;
+
+    cascade createCascade(cascade_desc cd) const;
+    std::vector<cascade> createCascades(cascade_desc desc0, int cascadeCount) const;
+
+    int getTotalProbeCount(std::vector<cascade> cascades) const;
+
+  public:
+    virtual void init(void *userData) override;
+    virtual void begin() override;
+    virtual void update(float deltaTime) override;
+
+  public:
+    [[nodisacrd]] inline const int getCascadeCount() const
+    {
+        return m_maxCascadeCount;
+    }
+    [[nodiscard]] inline const Buffer *getParametersBufferHandle() const
+    {
+        return m_radianceCascadesParametersBuffer.get();
+    }
+    [[nodiscard]] inline const Buffer *getCascadesDescBufferHandle() const
+    {
+        return m_cascadesDescBuffer.get();
+    }
+    [[nodiscard]] inline const Buffer *getProbePositionsBufferHandle() const
+    {
+        return m_probePositionBuffer.get();
+    }
+    [[nodiscard]] inline const Buffer *getRadianceIntervalsStorageBufferHandle(uint32_t inFlightCount) const
+    {
+        return m_radianceIntervalsStorageBufferRW[inFlightCount].get();
+    }
+};
